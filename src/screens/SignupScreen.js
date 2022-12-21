74e2817b0a4595a5e7e7961b089eb465a492108f
Smit Lakhani <smitlakhani13@gmail.com>
2022-12-21 17:36:39 +0530

    Initial

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const SignupScreen = () => {
+  return <Text style={{ fontSize: 48 }}>Signup Screen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default SignupScreen;
